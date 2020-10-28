914057ee2a7dc41399ad09d405a6fbd039d55cd4
SAFA <sammoudasaffa@gmail.com>
2020-10-27 22:05:45 -0400

    my first commit

---
+++ src/imported.ts
@@ -0,0 +1,5 @@
+console.log('We have been imported!');
+
+export const importedValue = '1234';
+
+console.log(Object.values({ a: 2, b: 3 }));
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import moment from 'moment';
+import { importedValue } from './imported';
+
+console.log(importedValue);
+
+console.log(moment().format('MMMM'));
