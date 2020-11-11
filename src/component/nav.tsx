92a4c80e92c5b488653b2fe8d2d9ab4f2890f2da
SAFA <sammoudasaffa@gmail.com>
2020-11-11 12:56:02 -0500

    header

--- src/component/nav.tsx
+++ src/component/nav.tsx
@@ -5,12 +5,20 @@ interface State { }
 export class Nav extends React.Component<Props, State> {
     public render() {
         return <div className='header'>
-            <div className=''>
-                <ul>
-                    {/* <Link to='/'>
-                        <li> Home </li>
-                    </Link> */}
-                </ul>
+            <div className='flex header__box container-big'>
+                <div className='header__logo'>
+                    <img src='../img/logo-effix.png' alt='logo' />
+                </div>
+                <div className='header__nav flex'>
+                    <nav className='main-nav'>
+                        <ul>
+                            <li>Test</li>
+                        </ul>
+                    </nav>
+                    <ul>
+                        <li>Alo</li>
+                    </ul>
+                </div>
             </div>
         </div>;
     }
