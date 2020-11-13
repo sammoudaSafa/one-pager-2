0c4709955a9bb57f75932b0c0bda695b17a183c9
SAFA <sammoudasaffa@gmail.com>
2020-11-12 21:33:17 -0500

    nouvelles table d objets

--- src/component/nouvelles.tsx
+++ src/component/nouvelles.tsx
@@ -9,9 +9,20 @@ export class Nouvelles extends React.Component<Props, State> {
     constructor(props: Props) {
         super(props);
         this.state = {
+            nouvelles: []
         };
     }
-
+    public componentDidMount() {
+        const nouvellesURL = 'https://effix-commerce.bianka.devwebgarneau.com/wp-json/wp/v2/posts';
+        // tslint:disable-next-line: no-floating-promises
+        fetch(nouvellesURL)
+            .then(response => response.json())
+            .then(response => {
+                this.setState({
+                    nouvelles: response
+                });
+            });
+    }
     public render() {
         return <div>
             <h2> Nouvelles</h2>
