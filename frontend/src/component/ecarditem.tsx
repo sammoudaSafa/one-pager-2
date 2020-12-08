a4aa6121c907c2617e6500c908a7df3a3bab0739
SAFA <sammoudasaffa@gmail.com>
2020-12-08 10:53:50 -0500

    affichage bouton

--- frontend/src/component/ecarditem.tsx
+++ frontend/src/component/ecarditem.tsx
@@ -27,7 +27,8 @@ export class ECardItem extends React.Component<Props, State> {
             <span> {this.props.card.date.toLocaleDateString(undefined /* 'fr-ca' */, dateFormat)}</span>
             <h2 className='heading-secondary u-margin-top-small u-margin-bottom-small'> {this.props.card.title.rendered}</h2>
             <p dangerouslySetInnerHTML={{ __html: this.props.card.excerpt.rendered }} />
-            <button className='btn'><a href={this.props.card.link}>Lire la suite</a></button>
+            {/* <button className='btn'><a href={this.props.card.link}>Lire la suite</a></button> */}
+            <a href={this.props.card.link} className='btn'>Lire la suite</a>
         </article>;
     }
 
